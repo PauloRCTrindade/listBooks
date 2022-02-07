7d0ddeaf63550aef7c4c36236c13b5937a347ed8
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2022-02-07 18:59:05 -0300

    Create index.jsx

---
+++ list-books/src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+import { Box, Header, HeaderLayout, TextField } from '@telefonica/mistica';
+import React from 'react';
+
+export default function HeaderApp() {
+
+  return (
+
+    <HeaderLayout 
+      header={
+        <Header title="List Books" />
+      }
+    >
+    </HeaderLayout>
+
+  )
+};
